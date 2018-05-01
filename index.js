0ba46823e304abe528e1694f12d45cf3f98027e5
vinu k.v <vinu.kuriyedath@gmail.com>
2018-05-01 20:43:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const app = express()
 
-var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
-    ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+var port = process.env.PORT || 8080 //process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip = process.env.IP || '0.0.0.0' // process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
