c2a5540631c411f62b62e91a51aa7ecd39006bb4
vinu k.v <vinu.kuriyedath@gmail.com>
2018-05-03 11:08:24 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.listen(port, ip);
 module.exports = app ;
 */
 
+/*
 var express = require('express'),
     fs = require('fs'),
     app = express();
@@ -20,12 +21,23 @@ var app = express();
  
 var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
  
- 
 app.get('/', function(req, res) {
     res.send('Hello from NodeJS  at '+ new Date());
 });
  
- 
 app.listen(8080, ip);
  
 module.exports = app;
+*/
+
+const express = require('express')
+const app = express()
+
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.IP || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
+
+app.get('/', (req, res) => res.send('Hello World Testing!'))
+
+app.listen(port, ip);
+
+module.exports = app ;
